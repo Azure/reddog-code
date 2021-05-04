26700278bb4bc92a5ae36125ecd2c7c8c09c3573
Lynn Orrell <lynn.orrell@microsoft.com>
2021-05-04 01:16:27 -0500

    Added AccountingService. Updated nuget packages.

---
+++ RedDog.AccountingService/Controllers/AccountingController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace RedDog.AccountingService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AccountingController : ControllerBase
+    {
+        private readonly ILogger<AccountingController> _logger;
+
+        public AccountingController(ILogger<AccountingController> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
