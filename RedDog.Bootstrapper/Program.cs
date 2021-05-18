391211a12eee21b3f43ae280e9b0816846f58cda
Lynn Orrell <Lynn.Orrell@microsoft.com>
2021-05-18 11:23:48 -0500

    Glo bootstrapperfix (#14)
    * Added retry logic in bootstrapper
    
    * Added retry logic in bootstrapper - take 2
    
    * Now shutting down dapr sidecar in bootstrapper.

--- RedDog.Bootstrapper/Program.cs
+++ RedDog.Bootstrapper/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Dapr.Client;
 using Microsoft.EntityFrameworkCore;
@@ -11,13 +12,17 @@ namespace RedDog.Bootstrapper
     class Program : IDesignTimeDbContextFactory<AccountingContext>
     {
         private const string SecretStoreName = "reddog.secretstore";
+        private string DaprHttpPort = Environment.GetEnvironmentVariable("DAPR_HTTP_PORT") ?? "3500";
 
         static async Task Main(string[] args)
         {
+            Console.WriteLine("Beginning EF Core migrations...");
             Program p = new Program();
 
             using AccountingContext context = p.CreateDbContext(null);
             await context.Database.MigrateAsync();
+            
+            Console.WriteLine("Migrations complete.");
         }
 
         public AccountingContext CreateDbContext(string[] args)
@@ -40,6 +45,35 @@ namespace RedDog.Bootstrapper
                     Task.Delay(5000).Wait();
                 }
             } while(connectionString == null);
+
+            bool isDaprShutdownSuccessful = false;
+            HttpClient httpClient = new HttpClient();
+            do
+            {
+                try
+                {
+                    var response = httpClient.PostAsync($"http://localhost:{DaprHttpPort}/v1.0/shutdown", null).Result;
+
+                    if(response.IsSuccessStatusCode)
+                    {
+                        isDaprShutdownSuccessful = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Unable to shutdown Dapr sidecare. Retrying in 5 seconds...");
+                        Console.WriteLine($"Dapr error message: {response.Content.ReadAsStringAsync().Result}");
+                    }
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine($"An exception occured while attempting to shutdown the Dapr sidecar.");
+                    Console.WriteLine(e.StackTrace);
+                }
+                finally
+                {
+                    Task.Delay(5000).Wait();
+                }
+            } while(!isDaprShutdownSuccessful);
 
             DbContextOptionsBuilder<AccountingContext> optionsBuilder = new DbContextOptionsBuilder<AccountingContext>().UseSqlServer(connectionString["reddog-sql"], b => 
             {
